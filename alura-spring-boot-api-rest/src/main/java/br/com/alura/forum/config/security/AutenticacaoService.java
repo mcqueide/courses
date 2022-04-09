fad8a7f111bfa7ecbb8663bfcd433bbdfa6e672b
John Mc.Queide <mcqueide@gmail.com>
2022-04-09 17:33:53 -0300

    - [Alura-Spring-Boot-API-REST] Aula 3 - Adicionando spring security

---
+++ alura-spring-boot-api-rest/src/main/java/br/com/alura/forum/config/security/AutenticacaoService.java
@@ -0,0 +1,21 @@
+package br.com.alura.forum.config.security;
+
+import br.com.alura.forum.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Dados inválidos"));
+    }
+}
